c91c05ba764b34dba271d63bf5a3eaf983dc12f2
余珩 <1281005689@qq.com>
2018-03-29 18:04:50 +0800

    saga entrance

---
+++ src/saga.js
@@ -0,0 +1,15 @@
+/**
+ * Saga entrance file
+ * @requires redux-saga
+ * @requires redux-form-saga
+ * @requires {@link module:Speed/Saga}
+ */
+import {all} from 'redux-saga/effects';
+
+/**
+ * [*rootSaga description]
+ * @yield {[type]} [description]
+ */
+export default function* rootSaga() {
+  yield all([]);
+}
