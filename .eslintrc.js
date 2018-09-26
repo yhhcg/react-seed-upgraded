a897f445e04fdc990119eba7b0db8f31dbd34df9
余珩 <1281005689@qq.com>
2018-09-26 10:21:53 +0800

    [core] Allow use of the void operator

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,5 +82,7 @@ module.exports = {
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/label-has-for': 'off',
     'jsx-a11y/no-autofocus': 'off', // We are a library, people do what they want.
+
+    'no-void': 'off',
   },
 };
